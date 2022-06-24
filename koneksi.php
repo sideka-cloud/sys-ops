36bc245266a088392069a6fcec9ab6e2f9a6c2c3
sideka-cloud <89623723+sideka-cloud@users.noreply.github.com>
2022-06-24 08:49:57 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 // isi nama host, username mysql, dan password mysql anda
-$konek = mysqli_connect("localhost","root","");
+$konek = mysqli_connect("localhost","root","P@ssw0rd!");
 
 // isikan dengan nama database yang akan di hubungkan
 $database = mysqli_select_db($konek, "barang_db");
